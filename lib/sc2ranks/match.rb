9595c8c7601afecb4f8d13fc0ade538a47dde901
Fred Kelly <freddy@staak.co.uk>
2012-07-11 11:38:27 +0100

    Match parser

--- lib/sc2ranks/match.rb
+++ lib/sc2ranks/match.rb
@@ -13,7 +13,25 @@ class Sc2ranks
     def self.all(bnet_url)
       uri = URI.parse(bnet_url + 'matches')
       response = Net::HTTP.get_response(uri)
-      puts uri
+      doc = Nokogiri::HTML(response.body)
+      rows = doc.css('.match-row')
+      rows.collect do |row|
+        match = {}
+        [
+          [:map, 'td[2]/text()'],
+          [:type, 'td[3]/text()'],
+          [:outcome, 'td[4]/span/text()', [:downcase, :to_sym]],
+          [:date, 'td[5]/text()', :to_date]
+        ].each do |name, xpath, methods|
+          match[name] = row.at_xpath(xpath).to_s.strip
+          unless methods.nil?
+            Array(methods).each do |method|
+              match[name] = match[name].send(method)
+            end
+          end
+        end
+        match # return the match
+      end
     end
   end
 end
